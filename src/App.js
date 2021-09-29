07861dad7cdad2271bba175b73fc09f17f345d5c
rafisfast <tah18245393@students.wolvcoll.ac.uk>
2021-09-29 18:47:08 +0100

    setup states in app, now next player can work

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class Square extends Component {
 
   render() {
     return (
-      <button className="square" onClick={() => {console.log("clicked"); this.setState({value: 'X'})}}> 
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}> 
+        {this.props.value}
       </button>
     )
   }
@@ -15,11 +15,20 @@ class Square extends Component {
 
 class App extends Component {
 
-  state = {}
+  state = {
+    squares: Array(9).fill(null)
+  }
+
+  clicked = (value) => {
+    console.log("clicked inside");
+    const sq = this.state.squares.slice();
+    sq[value] = "X";
+    this.setState({squares: sq});
+  }
 
   renderSquare = (value) => {
-    return <Square value={value} />
-  }
+    return <Square value={this.state.squares[value]} onClick={() => this.clicked(value)}/>
+  } 
 
   render() {
 
@@ -28,19 +37,19 @@ class App extends Component {
       <div className="app-container">
         <tr>Next player: X</tr>
         <tr>
+          {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
-          {this.renderSquare(3)}
         </tr>
         <tr>
+          {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
-          {this.renderSquare(6)}
         </tr>
         <tr>
+          {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
-          {this.renderSquare(9)}
         </tr>
       </div>
 
