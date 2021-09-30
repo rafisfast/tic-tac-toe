d263606d1594cbfcadd6cdaffab731e70358bca4
rafisfast <tah18245393@students.wolvcoll.ac.uk>
2021-09-30 13:47:41 +0100

    added winning condition

--- src/App.js
+++ src/App.js
@@ -13,17 +13,42 @@ class Square extends Component {
   }
 }
 
+function calculateWinner (squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  
+  for (let i = 0; i<lines.length; i++) {
+    const [a,b,c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null;
+}
+
 class App extends Component {
 
   state = {
-    squares: Array(9).fill(null)
+    squares: Array(9).fill(null),
+    XIsNext: false
   }
 
   clicked = (value) => {
     console.log("clicked inside");
     const sq = this.state.squares.slice();
-    sq[value] = "X";
-    this.setState({squares: sq});
+    if (calculateWinner(sq) && sq[value]) {
+      return;
+    }
+    sq[value] = this.state.XIsNext ? "X" : "O";
+    this.setState({squares: sq, XIsNext: !this.state.XIsNext});
   }
 
   renderSquare = (value) => {
@@ -32,17 +57,26 @@ class App extends Component {
 
   render() {
 
+    let status;
+    const winner = calculateWinner(this.state.squares.slice())
+
+    if (winner) {
+      status = "Winner is " + winner
+    } else {
+      status = "Next player:" + (this.state.XIsNext ? "X" : "O")
+    }
+
     return (
 
       <div className="app-container">
-        <tr>Next player: X</tr>
+        <tr>{status}</tr>
         <tr>
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </tr>
         <tr>
-          {this.renderSquare(3)}
+          {this.renderSquare(3)} 
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </tr>
